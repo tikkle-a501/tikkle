f74ff41b43b4c80421e9df4ffdabccf690233d79
ryucream612 <ryucream612@naver.com>
2024-09-02 11:25:04 +0900

    init: recoil 설치

--- front/tickle/src/app/layout.tsx
+++ front/tickle/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { RecoilRoot } from "recoil";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <RecoilRoot>{children}</RecoilRoot>
+      </body>
     </html>
   );
 }
