4b9c8cbb527018baec6bfa30d4a6ee114be8231b
glenn.syj <glenn.syj@gmail.com>
2024-09-03 14:08:36 +0900

    init: 초기 디렉토리 구조 설정
    S11P21A501-26

---
+++ back/tikkle/src/main/java/com/taesan/tikkle/TikkleApplication.java
@@ -0,0 +1,13 @@
+package com.taesan.tikkle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikkleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikkleApplication.class, args);
+	}
+
+}
---
+++ back/tikkle/src/test/java/com/taesan/tikkle/TikkleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taesan.tikkle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikkleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
