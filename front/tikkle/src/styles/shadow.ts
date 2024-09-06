154a02d1985221d1b6f8d87f56395a16b3ad8421
celeste7297 <dayoungpyo@gmail.com>
2024-09-06 11:11:42 +0900

    init: 기본 폴더 구조 변경 및 스타일 추가

---
+++ front/tikkle/src/styles/shadow.ts
@@ -0,0 +1,7 @@
+export const shadow = {
+    xs: '0px 12px 12px rgba(0, 0, 0, 0.25)',
+    s: '0px 4px 25px rgba(0, 0, 0, 0.1)',
+    m: '0px 4px 95px rgba(0, 0, 0, 0.1)',
+    l: '0px 30px 250px rgba(0, 0, 0, 0.3)',
+    lElevatedTop: '0px 60px 150px rgba(0, 0, 0, 0.3)',
+  };
