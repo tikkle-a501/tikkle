70249409f437d00ebb09b37191d743655363c774
celeste7297 <dayoungpyo@gmail.com>
2024-09-06 11:17:17 +0900

    init: 기본 폰트 설정 및 스타일 수정

---
+++ front/tikkle/src/styles/font.ts
@@ -0,0 +1,45 @@
+export const fontSize = {
+    60: '3.75rem',
+    40: '2.5rem',
+    34: '2.125rem',
+    28: '1.75rem',
+    24: '1.5rem',
+    22: '1.375rem',
+    20: '1.25rem',
+    18: '1.125rem',
+    17: '1.0625rem',
+    16: '1rem',
+    15: '0.9375rem',
+    14: '0.875rem',
+    13: '0.8125rem',
+    12: '0.75rem',
+  };
+  
+  export const fontWeight = {
+    700: '700',
+    600: '600',
+    400: '400',
+  };
+  
+  export const lineHeight = {
+    72: '4.5rem',
+    48: '3rem',
+    41: '2.5625rem',
+    34: '2.125rem',
+    30: '1.875rem',
+    28: '1.75rem',
+    26: '1.625rem',
+    25: '1.5625rem',
+    24: '1.5rem',
+    23: '1.4375rem',
+    22: '1.375rem',
+    21: '1.3125rem',
+    20: '1.25rem',
+    19: '1.1875rem',
+    18: '1.125rem',
+    16: '1rem',
+  };
+  
+  export const fontFamily = {
+    main: "'Pretendard', 'Noto Sans KR', 'Arial', 'Helvetica', 'sans-serif'",
+  };
