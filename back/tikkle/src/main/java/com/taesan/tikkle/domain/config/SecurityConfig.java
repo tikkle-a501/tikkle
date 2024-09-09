9f823605aba86e319e85bfedc7515452772852fb
glenn.syj <glenn.syj@gmail.com>
2024-09-09 17:19:45 +0900

    feat: SecurityFilterChain 빈 인증 인가  정책 설정
    CORS 활성화 및 CSRF 비활성화
    일차적 RequestMatcher 설정
    OAuth 대비 STATELESS session 정책 설정
    
    S11P21A501-92

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/config/SecurityConfig.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/config/SecurityConfig.java
@@ -4,10 +4,16 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+/**
+ * TODO: 클라이언트-서버 API 연동 시 사후적 수정 필요
+ */
 @Configuration
 public class SecurityConfig {
 
@@ -26,6 +32,33 @@ public class SecurityConfig {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http,
+		CorsConfigurationSource corsConfigurationSource) throws Exception {
+
+		http
+			.cors((cors) -> cors.configurationSource(corsConfigurationSource))
+			.csrf((csrf) -> csrf.disable());
+
+		http
+			.formLogin((auth) -> auth.disable());
+
+		// TODO: 개발 환경을 위한 패턴이므로 추후 작성 필요
+		String[] requestMatcherPatterns = new String[] {
+			"api/v1/**"
+		};
+
+		http
+			.authorizeHttpRequests((auth) -> auth
+				.requestMatchers(requestMatcherPatterns).permitAll()
+				.requestMatchers("/api/v1/manager").hasRole("MANAGER").anyRequest().authenticated());
+
+		http
+			.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		return http.build();
 	}
 
 }
