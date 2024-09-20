a7546582846ea312e00bb6bf72ffdd94507aaa2a
Lee9Bin <imagen33@daum.net>
2024-09-20 14:55:47 +0900

    feat: 한 시간 전 계산 메서드 구현
    S11P21A501-161

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/rate/service/RateService.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/rate/service/RateService.java
@@ -1,14 +1,14 @@
 package com.taesan.tikkle.domain.rate.service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.taesan.tikkle.domain.account.entity.ExchangeLog;
+import com.taesan.tikkle.domain.account.dto.ExchangeType;
 import com.taesan.tikkle.domain.account.service.AccountService;
+import com.taesan.tikkle.domain.rate.entity.Rate;
 import com.taesan.tikkle.domain.rate.repository.RateRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -22,20 +22,36 @@ public class RateService {
 	private final AccountService accountService;
 	private final RateRepository rateRepository;
 
-	private final double initialCoinToStarRate = 100.0;
-
 	@Transactional
 	@Scheduled(cron = "0 0 * * * *")
 	public void updateRate() {
+
+		LocalDateTime oneHourAgo = getOneHourAgo();
+
+		LocalDateTime startTime = oneHourAgo.withMinute(0).withSecond(0);
+		LocalDateTime endTime = oneHourAgo.withMinute(59).withSecond(59);
+
+		int totalTtoRAmount = accountService.getTotalQuantityByExchangeTypeAndPeriod(startTime, endTime,
+			ExchangeType.TTOR);
+		int totalRtoTAmount = accountService.getTotalQuantityByExchangeTypeAndPeriod(startTime, endTime,
+			ExchangeType.RTOT);
+
+		int newRate = calculateRate(totalTtoRAmount, totalRtoTAmount);
+
+		rateRepository.save(Rate.builder().timeToRank(newRate).build());
+	}
+
+	private LocalDateTime getOneHourAgo() {
 		LocalDateTime now = LocalDateTime.now();
-
 		LocalDateTime oneHourAgo = now.minusHours(1);
-		log.info("{}, {}", now, oneHourAgo);
+		log.info("현재 시간: {}, 한 시간 전: {}", now, oneHourAgo);
+		return oneHourAgo;
+	}
 
-		List<ExchangeLog> logs = accountService.findExchangeLogsBetween(oneHourAgo.withMinute(0).withSecond(0),
-			oneHourAgo.withMinute(59).withSecond(59), );
-
-		logs.forEach(log -> {
-		});
+	// 환율 계산 로직
+	private int calculateRate(int totalTtoRAmount, int totalRtoTAmount) {
+		// 기본적으로 totalTtoRAmount (시간 -> 포인트)와 totalRtoTAmount (포인트 -> 시간)의 비율을 기준으로 환율을 조정
+		int initialCoinToStarRate = 1000;
+		return initialCoinToStarRate;
 	}
 }
