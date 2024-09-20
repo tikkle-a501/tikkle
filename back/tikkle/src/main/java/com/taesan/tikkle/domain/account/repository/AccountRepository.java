4ed7023fce49cd28c72909a13aabf047fc766822
Lee9Bin <imagen33@daum.net>
2024-09-20 13:01:54 +0900

    feat: 환전량 메서드 수정
    S11P21A501-161

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/account/repository/AccountRepository.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/account/repository/AccountRepository.java
@@ -1,24 +1,21 @@
 package com.taesan.tikkle.domain.account.repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.taesan.tikkle.domain.account.dto.ExchangeType;
 import com.taesan.tikkle.domain.account.entity.Account;
-import com.taesan.tikkle.domain.account.entity.ExchangeLog;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
 
-	@Query("SELECT e FROM ExchangeLog e WHERE e.createdAt BETWEEN :startTime AND :endTime")
-	List<ExchangeLog> findAllExchangeLogs(LocalDateTime startTime, LocalDateTime endTime);
-
-	@Query("SELECT e FROM ExchangeLog e WHERE e.createdAt BETWEEN :startTime AND :endTime AND e.exchangeType = :exchangeType")
-	List<ExchangeLog> findExchangeLogsBetweenAndByType(LocalDateTime startTime, LocalDateTime endTime,
-		ExchangeType exchangeType);
+	@Query("SELECT SUM(e.quantity) FROM ExchangeLog e WHERE e.createdAt BETWEEN :startTime AND :endTime AND e.exchangeType = :exchangeType")
+	Integer getTotalQuantityByExchangeTypeAndPeriod(@Param("startTime") LocalDateTime startTime,
+		@Param("endTime") LocalDateTime endTime,
+		@Param("exchangeType") ExchangeType exchangeType);
 }
--- back/tikkle/src/main/java/com/taesan/tikkle/domain/account/service/AccountService.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/account/service/AccountService.java
@@ -1,12 +1,10 @@
 package com.taesan.tikkle.domain.account.service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.taesan.tikkle.domain.account.dto.ExchangeType;
-import com.taesan.tikkle.domain.account.entity.ExchangeLog;
 import com.taesan.tikkle.domain.account.repository.AccountRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -17,8 +15,8 @@ public class AccountService {
 
 	private final AccountRepository accountRepository;
 
-	public List<ExchangeLog> findExchangeLogsBetween(LocalDateTime startTime, LocalDateTime endTime,
+	public Integer getTotalQuantityByExchangeTypeAndPeriod(LocalDateTime startTime, LocalDateTime endTime,
 		ExchangeType exchangeType) {
-		return accountRepository.findExchangeLogsBetweenAndByType(startTime, endTime, exchangeType);
+		return accountRepository.getTotalQuantityByExchangeTypeAndPeriod(startTime, endTime, exchangeType);
 	}
 }
