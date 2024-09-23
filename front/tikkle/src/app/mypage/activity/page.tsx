d212ee615bef4a91e9377a755363495e3f34f35a
celeste7297 <celeste7297@naver.com>
2024-09-23 11:08:13 +0900

    feat: 마이페이지-나의 게시글 mock data 추가

--- front/tikkle/src/app/mypage/activity/page.tsx
+++ front/tikkle/src/app/mypage/activity/page.tsx
@@ -1,6 +1,42 @@
 import BoardCard from "@/components/card/BoardCard";
 
 export default function Activity() {
+  // mock data
+  const boardData = [
+    {
+      title: "title",
+      writer: "writer",
+      status: "done",
+      createdAt: "00000",
+      time: "2",
+      context: "Jorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      title: "title",
+      writer: "writer",
+      status: "done",
+      createdAt: "00000",
+      time: "2",
+      context: "Jorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      title: "title",
+      writer: "writer",
+      status: "done",
+      createdAt: "00000",
+      time: "2",
+      context: "Jorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      title: "title",
+      writer: "writer",
+      status: "done",
+      createdAt: "00000",
+      time: "2",
+      context: "djklfajdfljadlfajdflj",
+    },
+  ];
+
   return (
     <>
       <div className="flex flex-col gap-10">
@@ -14,148 +50,57 @@ export default function Activity() {
               게시 중인 글
             </span>
             <div className="scrollbar-hidden flex h-[44rem] flex-col gap-10 overflow-y-auto p-10">
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="Jorem ipsum dolor sit amet, consectetur adipiscing elit. NuncJorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc 
-"
-              />
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc 
-"
-              />
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="djklfajdfljadlfajdflj"
-              />
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="djklfajdfljadlfajdflj"
-              />
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="djklfajdfljadlfajdflj"
-              />
+              {boardData.map((item, index) => (
+                <BoardCard
+                  key={index}
+                  title={item.title}
+                  writer={item.writer}
+                  status={item.status}
+                  createdAt={item.createdAt}
+                  time={item.time}
+                  context={item.context}
+                />
+              ))}
             </div>
           </div>
 
+          {/* 진행 중인 글 */}
           <div className="flex flex-col gap-10">
             <span className="text-22 font-bold leading-28 text-teal-900">
               진행 중인 글
             </span>
             <div className="scrollbar-hidden flex h-[44rem] flex-col gap-10 overflow-y-auto p-10">
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="Jorem ipsum dolor sit amet, consectetur adipiscing elit. NuncJorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc 
-"
-              />
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc 
-"
-              />
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="djklfajdfljadlfajdflj"
-              />
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="djklfajdfljadlfajdflj"
-              />
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="djklfajdfljadlfajdflj"
-              />
+              {boardData.map((item, index) => (
+                <BoardCard
+                  key={index}
+                  title={item.title}
+                  writer={item.writer}
+                  status={item.status}
+                  createdAt={item.createdAt}
+                  time={item.time}
+                  context={item.context}
+                />
+              ))}
             </div>
           </div>
 
+          {/* 완료된 글 */}
           <div className="flex flex-col gap-10">
             <span className="text-22 font-bold leading-28 text-teal-900">
               완료된 글
             </span>
             <div className="scrollbar-hidden flex h-[44rem] flex-col gap-10 overflow-y-auto p-10">
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="Jorem ipsum dolor sit amet, consectetur adipiscing elit. NuncJorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc 
-"
-              />
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc 
-"
-              />
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="djklfajdfljadlfajdflj"
-              />
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="djklfajdfljadlfajdflj"
-              />
-              <BoardCard
-                title="title"
-                writer="writer"
-                status="done"
-                createdAt="00000"
-                time="2"
-                context="djklfajdfljadlfajdflj"
-              />
+              {boardData.map((item, index) => (
+                <BoardCard
+                  key={index}
+                  title={item.title}
+                  writer={item.writer}
+                  status={item.status}
+                  createdAt={item.createdAt}
+                  time={item.time}
+                  context={item.context}
+                />
+              ))}
             </div>
           </div>
         </div>
