7d80ade2dea047cc120bd31acb0d666ed527b717
glenn.syj <glenn.syj@gmail.com>
2024-09-24 12:56:56 +0900

    feat: Member ì엔티티 OneToMany ì매핑 제거
    매í ì엔í ê´기íS11P21A501-193

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/member/entity/Member.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/member/entity/Member.java
@@ -1,14 +1,10 @@
 package com.taesan.tikkle.domain.member.entity;
 
-import java.util.List;
 import java.util.UUID;
 
 import com.github.f4b6a3.ulid.UlidCreator;
 import com.taesan.tikkle.domain.account.entity.Account;
-import com.taesan.tikkle.domain.board.entity.Board;
-import com.taesan.tikkle.domain.chat.entity.Chatroom;
 import com.taesan.tikkle.domain.organization.entity.Organization;
-import com.taesan.tikkle.domain.review.entity.Review;
 import com.taesan.tikkle.global.entity.AuditableEntity;
 
 import jakarta.persistence.Column;
@@ -16,7 +12,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -27,7 +22,11 @@ import lombok.NoArgsConstructor;
 @Table(name = "members")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+/*
+TODO: Mattermost OAuth 관련 정보 추가 필요
+ */
 public class Member extends AuditableEntity {
+
 	@Id
 	@Column(columnDefinition = "BINARY(16)")
 	private UUID id = UlidCreator.getMonotonicUlid().toUuid();
@@ -39,23 +38,14 @@ public class Member extends AuditableEntity {
 	@OneToOne(mappedBy = "member")
 	private Account account;
 
-	@OneToMany(mappedBy = "member")
-	private List<Board> boards;
-
-	@OneToMany(mappedBy = "sender")
-	private List<Review> sentReviews;
-
-	@OneToMany(mappedBy = "receiver")
-	private List<Review> receivedReviews;
-
-	@OneToMany(mappedBy = "performer")
-	private List<Chatroom> chatrooms;
-
 	@Column(length = 64)
 	private String name;
 
 	@Column(length = 64)
 	private String nickname;
+
+	@Column
+	private String provider;
 
 	@Column(length = 128)
 	private String email;
