ad562eb4fe1c8fab7bbf754cce2e1187f9e37b10
Lee9Bin <imagen33@daum.net>
2024-09-26 00:43:31 +0900

    feat: 잔고 증감 메서드 구현
    예외 처리 메서드 구현
    
    S11P21A501-184

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/account/entity/Account.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/account/entity/Account.java
@@ -3,6 +3,7 @@ package com.taesan.tikkle.domain.account.entity;
 import java.util.UUID;
 
 import com.github.f4b6a3.ulid.UlidCreator;
+import com.taesan.tikkle.domain.account.dto.ExchangeType;
 import com.taesan.tikkle.domain.member.entity.Member;
 import com.taesan.tikkle.global.entity.AuditableEntity;
 
@@ -35,5 +36,23 @@ public class Account extends AuditableEntity {
 
 	@Column(name = "ranking_point", columnDefinition = "TINYINT UNSIGNED")
 	private Integer rankingPoint;
+
+	public void updateAccount(ExchangeType exchangeType, Integer rate, Integer quantity){
+		if (exchangeType.equals(ExchangeType.TTOR)){
+			this.timeQnt -= quantity;
+			this.rankingPoint += rate * quantity;
+			validTimeQntAndRankingPoint(timeQnt, rankingPoint);
+		}
+		else if (exchangeType.equals(ExchangeType.RTOT)){
+			this.timeQnt += quantity;
+			this.rankingPoint -= rate * quantity;
+			validTimeQntAndRankingPoint(timeQnt, rankingPoint);
+		}
+	}
+
+	private void validTimeQntAndRankingPoint(Integer timeQnt, Integer rankingPoint){
+		//TODO: 예외 정의 하기
+		if(timeQnt < 0 || rankingPoint < 0) throw new IllegalArgumentException("보유 잔고가 부족합니다.");
+	}
 }
 
