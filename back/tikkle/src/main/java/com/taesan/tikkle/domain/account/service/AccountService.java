e7e2d6a68d202cf4b6ba3ae8aec447583ee04d03
Lee9Bin <imagen33@daum.net>
2024-09-26 00:44:13 +0900

    feat: 계좌 업데이트 메서드 구현
    S11P21A501-184

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/account/service/AccountService.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/account/service/AccountService.java
@@ -2,6 +2,7 @@ package com.taesan.tikkle.domain.account.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.taesan.tikkle.domain.account.dto.ExchangeType;
 import com.taesan.tikkle.domain.account.dto.response.ExchangeLogFindAllResponse;
+import com.taesan.tikkle.domain.account.entity.Account;
 import com.taesan.tikkle.domain.account.repository.AccountRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -33,5 +35,13 @@ public class AccountService {
 			.stream()
 			.map(ExchangeLogFindAllResponse::from)
 			.collect(Collectors.toList());
+	}
+
+	public Account updateAccount(UUID accountId, ExchangeType exchangeType, Integer rate, Integer quantity) {
+		//TODO: 추후 예외 정의 하기
+		Account account = accountRepository.findById(accountId)
+			.orElseThrow(()-> new NoSuchElementException("계좌가 없습니다."));
+		account.updateAccount(exchangeType, rate, quantity);
+		return account;
 	}
 }
