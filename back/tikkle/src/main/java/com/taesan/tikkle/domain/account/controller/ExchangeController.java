0d750c9bb5568d7c7d3b41162c6231d3a045e30d
Lee9Bin <imagen33@daum.net>
2024-09-26 00:40:30 +0900

    feat: 환전 요청 api 구현
    S11P21A501-184

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/account/controller/ExchangeController.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/account/controller/ExchangeController.java
@@ -7,9 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.taesan.tikkle.domain.account.dto.request.ExchangeRequest;
 import com.taesan.tikkle.domain.account.dto.response.ExchangeLogFindAllResponse;
 import com.taesan.tikkle.domain.account.service.ExchangeService;
 
@@ -28,5 +31,13 @@ public class ExchangeController {
 		UUID memberId = UUID.fromString("01920dd1-423e-f86b-a4dd-28a20d81fab0");
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(exchangeService.getExchangeLogs(memberId));
+	}
+
+	@PostMapping
+	public ResponseEntity<?> exchange(@RequestBody ExchangeRequest exchangeRequest){
+		//TODO: 추후 사용자 인증 처리 로직 결정 되면 수정해야합니다.
+		UUID memberId = UUID.fromString("01920dd1-423e-f86b-a4dd-28a20d81fab0");
+		exchangeService.exchange(memberId, exchangeRequest);
+		return ResponseEntity.noContent().build();
 	}
 }
