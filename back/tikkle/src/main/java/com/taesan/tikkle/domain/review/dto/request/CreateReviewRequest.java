6b7d4a546239677987888a8649bfe342d57cef33
허동민 <gjehdals456@gmail.com>
2024-09-26 17:27:54 +0900

    feat : 리뷰 생성하기
    현재 로그인한 아이디가 약속에 따른 상대방에게 리뷰를 날립니다.
    
    S11P21A501-255

---
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/review/dto/request/CreateReviewRequest.java
@@ -0,0 +1,15 @@
+package com.taesan.tikkle.domain.review.dto.request;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateReviewRequest {
+	private UUID appointmentId;
+	private String type;
+}
