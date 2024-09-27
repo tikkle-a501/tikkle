a4acf1cd1f3ed6c3e3ef6c2309dcb04221766efc
glenn.syj <glenn.syj@gmail.com>
2024-09-27 16:14:40 +0900

    feat: refresh 토큰 발급 및 검증 구현
    Response로 json 보내기 위한 JacksonConfig 추가
    Redis에 Refresh Token 저장 및 업데이트
    
    S11P21A501-245

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/config/security/JwtAuthenticationFilter.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/config/security/JwtAuthenticationFilter.java
@@ -1,17 +1,23 @@
 package com.taesan.tikkle.domain.config.security;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taesan.tikkle.domain.member.service.CustomOAuth2UserService;
 import com.taesan.tikkle.domain.member.service.CustomUserDetailsService;
+import com.taesan.tikkle.domain.member.service.RedisTokenService;
+import com.taesan.tikkle.global.response.ApiResponse;
 import com.taesan.tikkle.global.utils.JwtUtil;
 
 import jakarta.servlet.FilterChain;
@@ -28,6 +34,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	private final JwtUtil jwtUtil;
 	private final CustomOAuth2UserService customOAuth2UserService;
 	private final CustomUserDetailsService customUserDetailsService;
+	private final RedisTokenService redisTokenService;
+	private final ObjectMapper objectMapper;
 	private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
 	@Override
@@ -36,19 +44,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 		logger.debug("cookies: {}", request.getCookies());
 
-		String jwtToken = null;
+		String accessCookieValue = null;
+		String refreshCookieValue = null;
 
-		// 쿠키에서 "Authorization" 쿠키를 찾음
+		// 쿠키에서 "Authorization", "refresh_token" 쿠키를 찾음
 		if (request.getCookies() != null) {
 			for (Cookie cookie : request.getCookies()) {
 				if ("Authorization".equals(cookie.getName())) {
-					jwtToken = cookie.getValue();
-					break;
+					accessCookieValue = cookie.getValue();
+				} else if ("refresh_token".equals(cookie.getName())) {
+					refreshCookieValue = cookie.getValue();
 				}
 			}
 		}
 
-		if (jwtToken == null) {
+		// 둘 중 하나라도 없으면 OAuth 인증 필요
+		if (accessCookieValue == null || refreshCookieValue == null) {
 			filterChain.doFilter(request, response);
 			logger.debug("jwt token null!");
 			return;
@@ -56,36 +67,61 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 		String username = null;
 		String accessToken = null;
+		String refreshToken = null;
 
-		if (!jwtToken.startsWith("Bearer+")) {
-			logger.debug("Invalid auth header: {}", jwtToken);
-			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid Authorization header.");
+		if (!accessCookieValue.startsWith("Bearer+")) {
+			ApiResponse<Object> errorResponse = ApiResponse.error(1711, "올바르지 않은 JWT 토큰 형식입니다.");
+			String jsonResponse = objectMapper.writeValueAsString(errorResponse);
+
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().write(jsonResponse);
 			return;
 		}
 
-		accessToken = jwtToken.substring(7);
+		accessToken = accessCookieValue.substring(7);
+		refreshToken = refreshCookieValue;
 		logger.debug("accessToken: {}", accessToken);
 		try {
 			username = jwtUtil.extractUsername(accessToken);
 		} catch (Exception e) {
-			logger.error("JWT 추출 실패: " + e.getMessage());
-			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token.");
+			ApiResponse<Object> errorResponse = ApiResponse.error(1711, "올바르지 않은 JWT 토큰 형식입니다.");
+			String jsonResponse = objectMapper.writeValueAsString(errorResponse);
+
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().write(jsonResponse);
 			return;
 		}
 
-		if (username == null) {
-			throw new IllegalArgumentException("No username found in the token.");
+		if (username == null || !username.equals(jwtUtil.extractUsername(refreshToken))) {
+			ApiResponse<Object> errorResponse = ApiResponse.error(1711, "올바르지 않은 JWT 토큰 형식입니다.");
+			String jsonResponse = objectMapper.writeValueAsString(errorResponse);
+
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().write(jsonResponse);
+			return;
 		}
 
 		CustomUserDetails userDetails = null;
 		try {
 			userDetails = (CustomUserDetails)customUserDetailsService.loadUserByUsername(username);
 		} catch (UsernameNotFoundException e) {
-			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "User not found.");
+			ApiResponse<Object> errorResponse = ApiResponse.error(1704, "존재하지 않는 회원입니다.");
+			String jsonResponse = objectMapper.writeValueAsString(errorResponse);
+
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().write(jsonResponse);
 			return;
 		}
-		if (jwtUtil.validateToken(accessToken, userDetails.getUsername())) {
 
+		if (jwtUtil.validateToken(accessToken, userDetails.getUsername())) {
 			/*
 				NOTE: Oauth2AuthenticationToken 역시 내부적으로 Credential을 ""로 이용.
 			 */
@@ -95,9 +131,69 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 			// SecurityContext에 인증 정보 설정
 			logger.debug("auth username: {}", ((CustomUserDetails)authentication.getPrincipal()).getMemberId());
 			SecurityContextHolder.getContext().setAuthentication(authentication);
+
+			logger.debug("passed!");
+			filterChain.doFilter(request, response);
 		}
 
-		logger.debug("passed!");
+		if (!refreshToken.equals(redisTokenService.getRefreshToken(userDetails.getUsername()))) {
+			ApiResponse<Object> errorResponse = ApiResponse.error(1710, "올바르지 않은 RefreshToken 입니다");
+			String jsonResponse = objectMapper.writeValueAsString(errorResponse);
+
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().write(jsonResponse);
+			return;
+		}
+
+		/*
+			NOTE: refreshToken은 유효하나 accessToken이 유효하지 않은 경우 AccessToken 재발급
+		 */
+		if (jwtUtil.validateToken(refreshToken, userDetails.getUsername())) {
+			UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+				userDetails, "", userDetails.getAuthorities());
+
+			// SecurityContext에 인증 정보 설정
+			logger.debug("auth username: {}", ((CustomUserDetails)authentication.getPrincipal()).getMemberId());
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+
+			logger.debug("passed!");
+
+			String newAccessToken = jwtUtil.generateToken(new HashMap<>(), userDetails.getUsername());
+
+			ResponseCookie jwtCookie
+				= ResponseCookie.from("Authorization", newAccessToken)
+				.maxAge(60 * 60)
+				.path("/")
+				.httpOnly(false)
+				.secure(false)
+				.build();
+
+			response.addHeader(HttpHeaders.SET_COOKIE, jwtCookie.toString());
+
+		} else {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		/*
+			NOTE: Refresh Token이 유효하면서, 유효기간의 절반이 지난 경우, 추가 처리.
+		 */
+		if (jwtUtil.isHalfLifePassed(refreshToken)) {
+			String newRefreshToken = jwtUtil.generateRefreshToken(new HashMap<>(), userDetails.getUsername());
+
+			ResponseCookie refreshTokenCookie
+				= ResponseCookie.from("Authorization", newRefreshToken)
+				.maxAge(60 * 60 * 24)
+				.path("/")
+				.httpOnly(false)
+				.secure(false)
+				.build();
+
+			response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+			redisTokenService.storeRefreshToken(userDetails.getMemberId(), newRefreshToken, 1000 * 60 * 60 * 24);
+		}
 
 		filterChain.doFilter(request, response); // 요청 처리 계속
 	}
--- back/tikkle/src/main/java/com/taesan/tikkle/domain/config/security/JwtOAuth2SuccessHandler.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/config/security/JwtOAuth2SuccessHandler.java
@@ -7,27 +7,28 @@ import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import com.taesan.tikkle.domain.member.service.RedisTokenService;
 import com.taesan.tikkle.global.utils.JwtUtil;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class JwtOAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
 	private static final Logger logger = LoggerFactory.getLogger(JwtOAuth2SuccessHandler.class);
 	private final JwtUtil jwtUtil;
-
-	public JwtOAuth2SuccessHandler(JwtUtil jwtUtil) {
-		this.jwtUtil = jwtUtil;
-	}
+	private final RedisTokenService redisTokenService;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request,
@@ -41,8 +42,7 @@ public class JwtOAuth2SuccessHandler implements AuthenticationSuccessHandler {
 		Map<String, Object> claims = new HashMap<>();
 
 		String jwt = jwtUtil.generateToken(claims, memberId.toString());
-
-
+		String refreshToken = jwtUtil.generateRefreshToken(claims, memberId.toString());
 		/*
 			FIXME: 배포 환경에서는 setSecure를 true로 바꿔줘야 함.
 		 */
@@ -54,7 +54,19 @@ public class JwtOAuth2SuccessHandler implements AuthenticationSuccessHandler {
 			.secure(false)
 			.build();
 
-		response.addHeader("Set-Cookie", jwtCookie.toString());
+		ResponseCookie refreshTokenCookie
+			= ResponseCookie.from("refresh_token", refreshToken)
+			.maxAge(60 * 60 * 24)
+			.path("/")
+			.httpOnly(false)
+			.secure(false)
+			.build();
+
+		response.addHeader(HttpHeaders.SET_COOKIE, jwtCookie.toString());
+		response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+
+		redisTokenService.storeRefreshToken(memberId, refreshToken, 1000 * 60 * 60 * 24);
+
 		response.sendRedirect("http://localhost:3000/home");
 	}
 
