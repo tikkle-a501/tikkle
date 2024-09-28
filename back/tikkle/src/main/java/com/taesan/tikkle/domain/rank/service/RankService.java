37f3fcdd64b9b3b76b7edff60289ba983eade296
Lee9Bin <imagen33@daum.net>
2024-09-29 01:25:45 +0900

    feat: 응답 형식 수정 및 order 설정 구현 추가적으로 매 시각 1분에 랭킹 업데이트 되도록 수정
    S11P21A501-186

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/rank/service/RankService.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/rank/service/RankService.java
@@ -2,15 +2,19 @@ package com.taesan.tikkle.domain.rank.service;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taesan.tikkle.domain.member.dto.response.MemberRankResponse;
 import com.taesan.tikkle.domain.member.service.MemberService;
+import com.taesan.tikkle.domain.rank.dto.RankResponse;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,17 +30,32 @@ public class RankService {
 
 	private static final String RANKING_KEY = "ranking";
 
-	public List<MemberRankResponse> getRankList() {
+	public RankResponse getRankList(UUID username) {
 		ZSetOperations<String, Object> stringObjectZSetOperations = redisTemplate.opsForZSet();
 		Long existingCount = stringObjectZSetOperations.size(RANKING_KEY);
 
 		if (isNotCache(existingCount)) {
 			existingCount = makeRankList(stringObjectZSetOperations);
 		}
-		return Objects.requireNonNull(stringObjectZSetOperations.reverseRange(RANKING_KEY, 1, existingCount + 1))
+
+		AtomicReference<MemberRankResponse> myRankRef = new AtomicReference<>();
+		AtomicInteger rankCounter = new AtomicInteger(1);
+
+		List<MemberRankResponse> rankList = Objects.requireNonNull(
+				stringObjectZSetOperations.reverseRange(RANKING_KEY, 1, existingCount + 1))
 			.stream()
 			.map(this::convertToMemberRankResponse)
-			.collect(Collectors.toList());
+			.peek(memberRank -> {
+				memberRank.grantOrder(rankCounter.getAndIncrement());
+				if (memberRank.getMemberId().equals(username)) {
+					myRankRef.set(memberRank);
+				}
+			})
+			.toList();
+
+		MemberRankResponse myRank = myRankRef.get();
+
+		return RankResponse.of(rankList, myRank);
 	}
 
 	private boolean isNotCache(Long existingCount) {
@@ -53,11 +72,15 @@ public class RankService {
 		return stringObjectZSetOperations.size(RANKING_KEY);
 	}
 
-	// @Scheduled(cron = "0/1 * * * * *")
+	@Scheduled(cron = "0 1 * * * *")
 	public void deleteRankList() {
 		redisTemplate.delete(RANKING_KEY);
 		log.info("기존 랭킹 데이터 삭제 완료");
+		ZSetOperations<String, Object> stringObjectZSetOperations = redisTemplate.opsForZSet();
+		makeRankList(stringObjectZSetOperations);
+		log.info("랭킹 업데이트 성공");
 	}
+
 	private MemberRankResponse convertToMemberRankResponse(Object data) {
 		return objectMapper.convertValue(data, MemberRankResponse.class);
 	}
