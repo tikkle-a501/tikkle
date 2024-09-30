f7d28f24bc8f4048704b1ec304cfb9344e91a022
ryucream612 <ryucream612@naver.com>
2024-09-30 13:52:10 +0900

    feat: 테스트용 환율 생성 코드 연동
    S11P21A501-268

--- front/tikkle/src/hooks/rate/index.ts
+++ front/tikkle/src/hooks/rate/index.ts
@@ -1,1 +1,2 @@
 export * from "./useFetchRate";
+export * from "./useCreateRate";
--- front/tikkle/src/libs/api/rate/index.ts
+++ front/tikkle/src/libs/api/rate/index.ts
@@ -4,3 +4,8 @@ import { handleApiRequest } from "../client";
 export const fetchRate = async () => {
   return handleApiRequest<RateGetResponses, "get">("/rate", "get");
 };
+
+// 새로운 환율 데이터를 생성하는 API 요청 함수(테스트용으로만 쓰임)
+export const createRate = async () => {
+  return handleApiRequest<void, "post">("/rate", "post");
+};
