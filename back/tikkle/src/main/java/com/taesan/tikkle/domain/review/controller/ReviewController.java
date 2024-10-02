f9bf1774d276d0b419daf18ce4b6b1676e836c3f
허동민 <gjehdals456@gmail.com>
2024-10-02 15:15:09 +0900

    fix : merge conflict resolve
    Front Merge Conflict Resolved
    
    S11P21A501-308-review-fix

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/review/controller/ReviewController.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/review/controller/ReviewController.java
@@ -1,15 +1,20 @@
 package com.taesan.tikkle.domain.review.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.taesan.tikkle.domain.review.dto.request.CreateReviewRequest;
+import com.taesan.tikkle.domain.review.dto.response.ReviewListResponse;
 import com.taesan.tikkle.domain.review.service.ReviewService;
+import com.taesan.tikkle.global.annotations.AuthedUsername;
+import com.taesan.tikkle.global.response.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,8 +25,16 @@ public class ReviewController {
 	private final ReviewService reviewService;
 
 	@PostMapping("")
-	public ResponseEntity<UUID> createReview(@RequestBody CreateReviewRequest request) {
-		return ResponseEntity.ok(reviewService.createReview(request));
+	public ResponseEntity<ApiResponse<UUID>> createReview(@RequestBody CreateReviewRequest request,
+		@AuthedUsername UUID memberId) {
+		return ResponseEntity.ok(
+			ApiResponse.success("리뷰가 성공적으로 생성되었습니다.", reviewService.createReview(request, memberId)));
 	}
 
+	// TODO : Get Review
+	@GetMapping("")
+	public ResponseEntity<ApiResponse<List<ReviewListResponse>>> getReviews(@AuthedUsername UUID memberId) {
+		ApiResponse<List<ReviewListResponse>> response = ApiResponse.success("리뷰 조회를 성공하였습니다.",reviewService.getReviews(memberId));
+		return ResponseEntity.ok(response);
+	}
 }
--- back/tikkle/src/main/java/com/taesan/tikkle/domain/review/service/ReviewService.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/review/service/ReviewService.java
@@ -1,6 +1,8 @@
 package com.taesan.tikkle.domain.review.service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,10 +12,12 @@ import com.taesan.tikkle.domain.appointment.repository.AppointmentRepository;
 import com.taesan.tikkle.domain.member.entity.Member;
 import com.taesan.tikkle.domain.member.repository.MemberRepository;
 import com.taesan.tikkle.domain.review.dto.request.CreateReviewRequest;
+import com.taesan.tikkle.domain.review.dto.response.ReviewListResponse;
 import com.taesan.tikkle.domain.review.entity.Review;
 import com.taesan.tikkle.domain.review.repository.ReviewRepository;
+import com.taesan.tikkle.global.errors.ErrorCode;
+import com.taesan.tikkle.global.exceptions.CustomException;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -24,17 +28,32 @@ public class ReviewService {
 	private final AppointmentRepository appointmentRepository;
 	private final MemberRepository memberRepository;
 
-	public UUID createReview(CreateReviewRequest request) {
+	public UUID createReview(CreateReviewRequest request, UUID senderId) {
 		Appointment appointment = appointmentRepository.findById(request.getAppointmentId()).orElseThrow(
-			EntityNotFoundException::new);
+			() -> new CustomException(ErrorCode.APPOINTMENT_NOT_FOUND));
 		Member performer = appointment.getRoom().getPerformer();
 		Member writer = appointment.getRoom().getWriter();
-		// TODO : refresh token
-		UUID senderId = UUID.randomUUID();
 		Member receiver = (senderId.equals(performer.getId()) ? writer : performer);
-		Member sender = memberRepository.findById(senderId).orElseThrow(EntityNotFoundException::new);
-		Review review = new Review(sender,receiver,request.getType());
+		Member sender = memberRepository.findById(senderId)
+			.orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+		Review review = new Review(sender, receiver, request.getType());
 		reviewRepository.save(review);
 		return review.getId();
 	}
+
+	public List<ReviewListResponse> getReviews(UUID memberId) {
+		Member receiver = memberRepository.findById(memberId)
+			.orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+
+		List<Review> reviews = reviewRepository.findByReceiver(receiver);
+
+		// reviews를 List<ReviewListResponse>로 변환
+		return reviews.stream()
+			.map(review -> new ReviewListResponse(
+				review.getId(),
+				review.getType()
+			))
+			.collect(Collectors.toList());
+	}
+
 }
