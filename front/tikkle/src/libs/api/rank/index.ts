f26dc7d57077472fbe44c72d90a9d5067f20dabe
ryucream612 <ryucream612@naver.com>
2024-10-04 18:38:06 +0900

    fix: 잘못된 경로 수정
    S11P21A501-320

--- front/tikkle/src/libs/api/rank/index.ts
+++ front/tikkle/src/libs/api/rank/index.ts
@@ -9,7 +9,7 @@ export const fetchRank = async () => {
 // 랭킹 검색 (keyword 사용)
 export const searchRank = async (keyword: string) => {
   return handleApiRequest<RankSearchGetResponses, "get">(
-    `/api/v1/rank/search?keyword=${encodeURIComponent(keyword)}`,
+    `/rank/search?keyword=${encodeURIComponent(keyword)}`,
     // 특수 문자가 포함될 수 있으므로 encodeURIComponent 함수를 사용해 안전하게 URL 인코딩을 적용
     "get",
   );
