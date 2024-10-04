659744b699f964c19ffe0d9f7a519f9259c0ef3c
dayoungpyo <dayoungpyo@gmail.com>
2024-10-04 16:50:11 +0900

    feat: 마이페이지-나의 거래내역 수정

--- front/tikkle/src/app/(after-login)/mypage/activity/page.tsx
+++ front/tikkle/src/app/(after-login)/mypage/activity/page.tsx
@@ -2,54 +2,20 @@ import BoardCard from "@/components/card/BoardCard";
 
 export default function Activity() {
   // mock data
-  const boardData = [
-    {
-      title: "title",
-      writer: "writer",
-      status: "done",
-      createdAt: "00000",
-      time: 2,
-      context: "Jorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    },
-    {
-      title: "title",
-      writer: "writer",
-      status: "done",
-      createdAt: "00000",
-      time: 2,
-      context: "Jorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    },
-    {
-      title: "title",
-      writer: "writer",
-      status: "done",
-      createdAt: "00000",
-      time: 2,
-      context: "Jorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    },
-    {
-      title: "title",
-      writer: "writer",
-      status: "done",
-      createdAt: "00000",
-      time: 2,
-      context: "djklfajdfljadlfajdflj",
-    },
-  ];
 
   return (
-    <div className="flex flex-col gap-10">
+    <div className="flex w-full flex-col gap-10">
       <span className="py-6 text-28 font-bold leading-34 text-teal-900">
         나의 게시글
       </span>
       <div className="flex flex-row gap-10 px-10">
         {/* 게시중인 글 */}
-        <div className="flex flex-col gap-10">
+        <div className="flex flex-1 flex-col gap-10">
           <span className="text-22 font-bold leading-28 text-teal-900">
             게시 중인 글
           </span>
           <div className="scrollbar-hidden flex h-[44rem] flex-col gap-10 overflow-y-auto p-10">
-            {boardData.map((item, index) => (
+            {/* {boardData.map((item, index) => (
               <BoardCard
                 boardId={"1"}
                 key={index}
@@ -60,17 +26,17 @@ export default function Activity() {
                 time={item.time}
                 content={item.context}
               />
-            ))}
+            ))} */}
           </div>
         </div>
 
         {/* 진행 중인 글 */}
-        <div className="flex flex-col gap-10">
+        <div className="flex flex-1 flex-col gap-10">
           <span className="text-22 font-bold leading-28 text-teal-900">
             진행 중인 글
           </span>
           <div className="scrollbar-hidden flex h-[44rem] flex-col gap-10 overflow-y-auto p-10">
-            {boardData.map((item, index) => (
+            {/* {boardData.map((item, index) => (
               <BoardCard
                 boardId={"1"}
                 key={index}
@@ -81,17 +47,17 @@ export default function Activity() {
                 time={item.time}
                 content={item.context}
               />
-            ))}
+            ))} */}
           </div>
         </div>
 
         {/* 완료된 글 */}
-        <div className="flex flex-col gap-10">
+        <div className="flex flex-1 flex-col gap-10">
           <span className="text-22 font-bold leading-28 text-teal-900">
             완료된 글
           </span>
           <div className="scrollbar-hidden flex h-[44rem] flex-col gap-10 overflow-y-auto p-10">
-            {boardData.map((item, index) => (
+            {/* {boardData.map((item, index) => (
               <BoardCard
                 boardId={"1"}
                 key={index}
@@ -102,7 +68,7 @@ export default function Activity() {
                 time={item.time}
                 content={item.context}
               />
-            ))}
+            ))} */}
           </div>
         </div>
       </div>
