5947fd4e22350b547bdfb35e3cb693af3b76aed7
허동민 <gjehdals456@gmail.com>
2024-10-04 13:12:56 +0900

    feat : log 추가 및 어노테이션 추가
    log와 repository 어노테이션 추가
    
    S11P21A501-315-chat-error

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/chat/repository/ChatRepository.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/chat/repository/ChatRepository.java
@@ -5,9 +5,11 @@ import java.util.UUID;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.taesan.tikkle.domain.chat.entity.Chat;
 
+@Repository
 public interface ChatRepository extends MongoRepository<Chat, ObjectId> {
 	// roomId에 해당하는 최신 Chat을 timestamp 기준으로 가져오기
 	Chat findTopByChatroomIdOrderByTimestampDesc(String chatroomId);
