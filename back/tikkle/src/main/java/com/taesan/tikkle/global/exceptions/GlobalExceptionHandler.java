896aaf472be86995f9e86fe201f6657b756fccbc
HurDong <gjehdals456@gmail.com>
2024-10-06 07:00:38 +0900

    feat : error handler 추가
    어떤 json에서 에러가 났는지 확인하기 위해 추가
    
    S11P21A501-324

---
+++ back/tikkle/src/main/java/com/taesan/tikkle/global/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.taesan.tikkle.global.exceptions;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(JsonMappingException.class)
+    public ResponseEntity<Object> handleJsonMappingException(JsonMappingException ex, WebRequest request) {
+        // 예외 발생 시 로그 남기기
+        ex.printStackTrace();
+
+        // 오류 발생 위치 정보 가져오기
+        StringBuilder errorMessage = new StringBuilder("@@@@@@@@@@@@@@@@ JSON Mapping Error: " + ex.getOriginalMessage());
+
+        // 오류 발생한 JSON 필드 경로 추가
+        if (!ex.getPath().isEmpty()) {
+            errorMessage.append(" at ");
+            ex.getPath().forEach(reference -> errorMessage.append(reference.getFieldName()).append(" "));
+        }
+
+        // 오류 메시지 반환
+        return new ResponseEntity<>(errorMessage.toString(), HttpStatus.BAD_REQUEST);
+    }
+}
