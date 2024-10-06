18c033e650f88c2bc168ad8a657b1d2968c8284e
glenn-syj <glenn.syj@gmail.com>
2024-10-07 02:03:42 +0900

    fix: 기본 Bean 대신 이용하도록 Primary 추가
    S11P21A501-326

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/config/MessageConverterConfig.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/config/MessageConverterConfig.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 
 @Configuration
 public class MessageConverterConfig {
 
     @Bean
+    @Primary
     @DependsOn("objectMapper")
     public MappingJackson2MessageConverter mappingJackson2MessageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
