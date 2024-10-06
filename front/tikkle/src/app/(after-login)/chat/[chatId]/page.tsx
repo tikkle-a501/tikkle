b944522cab266224a927eee994d9c80107a3a035
glenn-syj <glenn.syj@gmail.com>
2024-10-06 23:56:07 +0900

    fix: delete Stringify
    S11P21A501-326

--- front/tikkle/src/app/(after-login)/chat/[chatId]/page.tsx
+++ front/tikkle/src/app/(after-login)/chat/[chatId]/page.tsx
@@ -101,7 +101,7 @@ export default function ChatId() {
 
       const sendMessage = {
         destination: "/app/sendMessage",
-        body: JSON.stringify(chatMessage),
+        body: chatMessage,
       };
       console.log("chatMessage는 말이죠 : ", chatMessage);
       console.log(
