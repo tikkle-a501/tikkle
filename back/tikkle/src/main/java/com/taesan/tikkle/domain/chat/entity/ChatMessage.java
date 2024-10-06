16ed5c62ee2b2c3b33572f7db412d78eab47683a
glenn-syj <glenn.syj@gmail.com>
2024-10-07 02:48:28 +0900

    fix: JsonCreator 추가
    S11P21A501-326

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/chat/entity/ChatMessage.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/chat/entity/ChatMessage.java
@@ -1,5 +1,7 @@
 package com.taesan.tikkle.domain.chat.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.taesan.tikkle.domain.config.UUIDDeserializer;
 import lombok.AllArgsConstructor;
@@ -12,13 +14,21 @@ import java.util.UUID;
 
 @Getter
 @Setter
-@AllArgsConstructor
+//@AllArgsConstructor
 @NoArgsConstructor
 public class ChatMessage {
-    @JsonDeserialize(using = UUIDDeserializer.class)
+//    @JsonDeserialize(using = UUIDDeserializer.class)
     private UUID chatroomId;
     //	private UUID senderId;
     private String content;
+
+    @JsonCreator
+    public ChatMessage(
+            @JsonProperty("chatroomId") UUID chatroomId,
+            @JsonProperty("content") String content) {
+        this.chatroomId = chatroomId;
+        this.content = content;
+    }
 
     @Override
     public String toString() {
