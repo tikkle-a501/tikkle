c13788ecad650d69f27a702b9772c7971b04c5bf
Lee9Bin <imagen33@daum.net>
2024-10-07 17:41:22 +0900

    feat: 환율 변동폭 수정
    S11P21A501-346

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/rate/service/RateService.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/rate/service/RateService.java
@@ -28,7 +28,7 @@ import lombok.extern.slf4j.Slf4j;
 public class RateService {
 
 	private final static int INITIAL_TIME_TO_RANK_POINT = 1000;
-	private final static double RATE_ADJUSTMENT_FACTOR = 0.03;
+	private final static double RATE_ADJUSTMENT_FACTOR = 0.01;
 
 	private final AccountService accountService;
 	private final RateRepository rateRepository;
@@ -126,9 +126,9 @@ public class RateService {
 		//거래량 비율 -> 분자와 분모의 차이가 커질 수록 한 쪽으로의 수요가 커지기 때문에 1을 빼서 반전
 		double demandDiscrepancyRatio = 1 - ((double)numerator / (double)denominator);
 		//거래량에 따른 가중치 값
-		double transactionVolumeImpact = Math.log(numerator + denominator);
+		double transactionVolumeImpact = Math.log(denominator) / Math.log(numerator + denominator);
 
-		return (int)Math.round(rateChangeLimit * demandDiscrepancyRatio + transactionVolumeImpact);
+		return (int)Math.round(rateChangeLimit * demandDiscrepancyRatio * transactionVolumeImpact);
 	}
 
 	private List<RateResponse> findAllResponses(boolean sortByCreatedAtAsc) {
