3984295585d5d0ed2ed1b2c6584bab868bcee6a6
허동민 <gjehdals456@gmail.com>
2024-10-07 16:05:46 +0900

    feat : kafkaTemplate Value 값 변환
    String 에서 ChatMessageResponse로
    
    S11P21A501-328

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/chat/service/KafkaProducer.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/chat/service/KafkaProducer.java
@@ -1,5 +1,7 @@
 package com.taesan.tikkle.domain.chat.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -15,7 +17,7 @@ import com.taesan.tikkle.domain.chat.repository.ChatRepository;
 @Service
 public class KafkaProducer {
 	@Autowired
-	private KafkaTemplate<String, ChatMessageResponse> kafkaTemplate;
+	private KafkaTemplate<String, String> kafkaTemplate;
 
 	@Autowired
 	private ChatRepository chatRepository;
@@ -23,22 +25,42 @@ public class KafkaProducer {
 	@Autowired
 	private ObjectMapper objectMapper;
 
+	static final Logger logger = LoggerFactory.getLogger(KafkaProducer.class);
+
 	@Transactional
 	public void sendMessage(ChatMessage chatMessage) {
+		// Logger 생성
+
 		Chat chat = new Chat(chatMessage.getChatroomId(), chatMessage.getSenderId(), chatMessage.getContent());
 		chatRepository.save(chat);
+
+		// 로그: 메시지 저장 완료
+		logger.info("ChatMessage 저장 완료. ChatroomId: {}, SenderId: {}, Content: {}",
+			chatMessage.getChatroomId(), chatMessage.getSenderId(), chatMessage.getContent());
+
 		// ChatMessageResponse 객체 생성
-		ChatMessageResponse response = new ChatMessageResponse(chatMessage.getContent(), chat.getTimestamp(),
-			chatMessage.getSenderId());
+		ChatMessageResponse response = new ChatMessageResponse(chatMessage.getContent(), chat.getTimestamp(), chatMessage.getSenderId());
+
 		try {
+			// 로그: 직렬화 시작
+			logger.info("ChatMessageResponse 객체 직렬화 시작: {}", response);
+
 			// ObjectMapper를 사용하여 ChatMessageResponse 객체를 JSON으로 직렬화
 			String responseJson = objectMapper.writeValueAsString(response);
+
+			// 로그: 직렬화된 JSON 메시지 출력
+			logger.info("직렬화된 JSON: {}", responseJson);
+
 			// 직렬화된 JSON 메시지를 Kafka에 전송
-			kafkaTemplate.send("chatroom." + chatMessage.getChatroomId(), response);
+			kafkaTemplate.send("chatroom." + chatMessage.getChatroomId(), responseJson);
+
+			// 로그: Kafka 메시지 전송 완료
+			logger.info("Kafka 메시지 전송 완료. 토픽: chatroom.{}", chatMessage.getChatroomId());
+
 		} catch (JsonProcessingException e) {
-			// 직렬화 오류 처리
+			// 로그: 직렬화 오류 발생
+			logger.error("JSON 직렬화 중 오류 발생: {}", e.getMessage());
 			e.printStackTrace();
-			// 필요하다면 예외 처리 로직 추가
 		}
 	}
 }
