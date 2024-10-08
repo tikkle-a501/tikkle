2efd287899c5a5084fdb05ca385d07e479711e72
ryucream612 <ryucream612@naver.com>
2024-10-08 15:46:24 +0900

    feat: 거래 완료하기 버튼 렌더링, 기능 구현
    S11P21A501-372

--- front/tikkle/src/types/review/index.d.ts
+++ front/tikkle/src/types/review/index.d.ts
@@ -1,13 +1,11 @@
 // 리뷰 작성 요청
 export interface ReviewCreateRequest {
   type: string;
-  content: string;
 }
 
 // 리뷰 조회 응답
 export interface ReviewGetResponse {
   type: string;
-  content: string;
 }
 
 export interface ReviewGetResponses {
