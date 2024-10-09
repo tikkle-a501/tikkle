643ccfc82dac7e152e5b3f11bb12106ef6ef000a
glenn.syj <glenn.syj@gmail.com>
2024-10-09 16:07:56 +0900

    feat: 이미지 제공 기본 환경 작성
    S11P21A501-377

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/config/WebConfig.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/config/WebConfig.java
@@ -2,14 +2,19 @@ package com.taesan.tikkle.domain.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.taesan.tikkle.global.resolver.AuthedUsernameArgumentResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+	@Value("${file.upload.image-dir}")
+	private String imageUploadDir;
 
 	private final AuthedUsernameArgumentResolver currentUserIdArgumentResolver;
 
@@ -21,4 +26,11 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(currentUserIdArgumentResolver);
 	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**")
+			.addResourceLocations("file:" + imageUploadDir + "/");
+	}
+
 }
