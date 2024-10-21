2f99686a1ca4a92e80de53d1638cf29b2149ef80
Lee9Bin <imagen33@daum.net>
2024-10-21 22:50:53 +0900

    feat: api/v2 구현 native 쿼리를 통해 limit, order by절 메서드 조회 API 구현
    S11P21A501-404

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/rank/controller/RankController.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/rank/controller/RankController.java
@@ -1,5 +1,6 @@
 package com.taesan.tikkle.domain.rank.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.taesan.tikkle.domain.member.dto.response.MemberRankProjection;
 import com.taesan.tikkle.domain.rank.dto.response.RankBaseResponse;
 import com.taesan.tikkle.domain.rank.service.RankService;
 import com.taesan.tikkle.global.annotations.AuthedUsername;
@@ -22,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class RankController {
 	private final RankService rankService;
 
+	//1. jpql 조인 전체 데이터 + 레디스 sorted set
 	@GetMapping("/v1/rank")
 	public ResponseEntity<ApiResponse<RankBaseResponse>> getMemberRanking(@AuthedUsername UUID username) {
 		ApiResponse<RankBaseResponse> response = ApiResponse.success("랭킹 조회에 성공했습니다.",
@@ -29,11 +32,11 @@ public class RankController {
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
+	//2. nativeQuery를 활용해 limit, order by 활용해 데이터 조회
 	@GetMapping("/v2/rank")
-	public ResponseEntity<ApiResponse<RankBaseResponse>> getMemberRanking(@AuthedUsername UUID username,
-		@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int size) {
-		ApiResponse<RankBaseResponse> response = ApiResponse.success("랭킹 조회에 성공했습니다.",
-			rankService.getRankList(username, page, size));
+	public ResponseEntity<ApiResponse<List<MemberRankProjection>>> getMemberRanking(@AuthedUsername UUID username, @RequestParam(required = false, defaultValue = "20") int limit, @RequestParam(required = false, defaultValue = "0") int offset) {
+		ApiResponse<List<MemberRankProjection>> response = ApiResponse.success("랭킹 조회에 성공했습니다.",
+			rankService.getRankList(limit, offset));
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
