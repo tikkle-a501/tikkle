c807a03f0e53407c50159b62cf86975df77c34d6
HurDong <happycodingmin@gmail.com>
2024-12-28 16:44:59 +0900

    refactor : autowired 에서 requriedargsconstructor 수정 및 채팅방 생성 로직 리팩토링
    tikkle-4

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/chat/service/ChatroomService.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/chat/service/ChatroomService.java
@@ -1,17 +1,5 @@
 package com.taesan.tikkle.domain.chat.service;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.taesan.tikkle.domain.board.entity.Board;
 import com.taesan.tikkle.domain.board.repository.BoardRepository;
 import com.taesan.tikkle.domain.chat.dto.request.CreateChatroomRequest;
@@ -28,40 +16,63 @@ import com.taesan.tikkle.domain.member.entity.Member;
 import com.taesan.tikkle.domain.member.repository.MemberRepository;
 import com.taesan.tikkle.global.errors.ErrorCode;
 import com.taesan.tikkle.global.exceptions.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class ChatroomService {
 
 	private static final Logger logger = LoggerFactory.getLogger(ChatroomService.class);
 
-	@Autowired
-	private ChatroomRepository chatroomRepository;
-	@Autowired
-	private ChatRepository chatRepository;
-	@Autowired
-	private BoardRepository boardRepository;
-	@Autowired
-	private MemberRepository memberRepository;
-	@Autowired
-	private FileService fileService;
+	private final ChatroomRepository chatroomRepository;
+
+	private final ChatRepository chatRepository;
+
+	private final BoardRepository boardRepository;
+
+	private final MemberRepository memberRepository;
+
+	private final FileService fileService;
 
 	@Transactional
 	public CreateChatroomResponse createChatroom(CreateChatroomRequest request, UUID memberId) {
+		// 게시글 조회
 		Board board = boardRepository.findById(request.getBoardId())
-			.orElseThrow(() -> new CustomException(ErrorCode.BOARD_NOT_FOUND));
+				.orElseThrow(() -> new CustomException(ErrorCode.BOARD_NOT_FOUND));
+
+		// 게시글 작성자 조회
 		Member writer = board.getMember();
+
+		// 약속 참여자 조회
 		Member performer = memberRepository.findById(memberId)
-			.orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
-		if (chatroomRepository.findByBoardIdAndWriterIdAndPerformerId(request.getBoardId(), writer.getId(),
-			performer.getId()).isPresent()) {
-			return new CreateChatroomResponse(
-				chatroomRepository.findByBoardIdAndWriterIdAndPerformerId(request.getBoardId(), writer.getId(),
-					performer.getId()).get().getId());
+				.orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+
+		// 동일한 조건의 채팅방이 존재하는 지 조회
+		Optional<Chatroom> existingChatroom = chatroomRepository.findByBoardIdAndWriterIdAndPerformerId(
+				request.getBoardId(), writer.getId(), performer.getId());
+
+		// 이미 존재한 경우 기존 채팅방 ID 반환
+		if (existingChatroom.isPresent()) {
+			return new CreateChatroomResponse(existingChatroom.get().getId());
 		}
+
+		// 존재하지 않은 경우 새로운 채팅방 생성
 		Chatroom chatroom = new Chatroom(board, performer, writer);
+
+		// MariaDB에 채팅방 저장
 		chatroomRepository.save(chatroom);
+
+		// 생성된 채팅방 ID 반환
 		return new CreateChatroomResponse(chatroom.getId());
 	}
+
 
 	@Transactional(readOnly = true)
 	public List<DetailChatroomResponse> getChatrooms(UUID memberId) {
@@ -71,12 +82,12 @@ public class ChatroomService {
 		extractChatroomDetails(chatroomRepository.findByPerformerId(memberId), responses, false);
 		// 가장 최신인 메세지를 갖는 채팅방을 앞으로
 		responses.sort(Comparator.comparing(DetailChatroomResponse::getLastMsgTime,
-			Comparator.nullsLast(Comparator.reverseOrder())));
+				Comparator.nullsLast(Comparator.reverseOrder())));
 		return responses;
 	}
 
 	private void extractChatroomDetails(List<Chatroom> chatrooms, List<DetailChatroomResponse> responses,
-		boolean isWriter) {
+										boolean isWriter) {
 		for (Chatroom chatroom : chatrooms) {
 			Chat lastChat = chatRepository.findTopByChatroomIdOrderByTimestampDesc(chatroom.getId().toString());
 			Member partner = isWriter ? chatroom.getPerformer() : chatroom.getWriter();
@@ -86,18 +97,18 @@ public class ChatroomService {
 
 			if (lastChat != null) {
 				Member lastSender = memberRepository.findByIdAndDeletedAtIsNull(
-						(UUID.fromString(lastChat.getSenderId())))
-					.orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+								(UUID.fromString(lastChat.getSenderId())))
+						.orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
 
 				// 대화 상대에 따라 performer와 writer 구분
 
 				responses.add(new DetailChatroomResponse(
-					chatroom.getId(),
-					partnerName,
-					partnerImage,
-					lastSender.getName(),
-					lastChat.getContent(),
-					lastChat.getTimestamp()
+						chatroom.getId(),
+						partnerName,
+						partnerImage,
+						lastSender.getName(),
+						lastChat.getContent(),
+						lastChat.getTimestamp()
 				));
 			} else {
 				responses.add(new DetailChatroomResponse(chatroom.getId(), partnerName, partnerImage));
@@ -113,11 +124,11 @@ public class ChatroomService {
 		logger.info("채팅방 입장 시도 - roomId: {}, memberId: {}", roomId, memberId);
 
 		Chatroom chatroom = chatroomRepository.findById(roomId)
-			.orElseThrow(() -> new CustomException(ErrorCode.CHATROOM_NOT_FOUND));
+				.orElseThrow(() -> new CustomException(ErrorCode.CHATROOM_NOT_FOUND));
 
 		// 로그: chatroom 정보 출력
 		logger.info("채팅방 찾음 - roomId: {}, 작성자: {}, 참가자: {}", roomId, chatroom.getWriter().getId(),
-			chatroom.getPerformer().getId());
+				chatroom.getPerformer().getId());
 
 		if (!memberId.equals(chatroom.getWriter().getId()) && !memberId.equals(chatroom.getPerformer().getId())) {
 			logger.warn("채팅방 입장 권한 없음 - memberId: {}", memberId);
@@ -129,10 +140,10 @@ public class ChatroomService {
 		List<Chat> cs = chatRepository.findByChatroomIdOrderByTimestampAsc(roomId.toString());
 		logger.info("변환 전 Chats : {} ", cs);
 		List<ChatResponse> chats = chatRepository.findByChatroomIdOrderByTimestampAsc(roomId.toString())
-			.stream()
-			.map(
-				chat -> new ChatResponse((UUID.fromString(chat.getSenderId())), chat.getContent(), chat.getTimestamp()))
-			.collect(Collectors.toList());
+				.stream()
+				.map(
+						chat -> new ChatResponse((UUID.fromString(chat.getSenderId())), chat.getContent(), chat.getTimestamp()))
+				.collect(Collectors.toList());
 
 		// 로그: 조회된 채팅 목록의 크기 출력
 		logger.info("조회된 채팅 수: {}", chats.size());
@@ -145,12 +156,12 @@ public class ChatroomService {
 		}
 
 		Member partner = chatroom.getWriter().getId().equals(memberId) ? chatroom.getPerformer() :
-			chatroom.getWriter();
+				chatroom.getWriter();
 
 		return new EnterChatroomResponse(chats, chatroom.getBoard().getMember().getId(),
-			partner.getName(), fileService.getProfileImage(partner.getId()), chatroom.getBoard().getStatus(),
-			chatroom.getBoard().getTitle(), chatroom.getBoard().getId(), chatroom.getBoard().getMember().getId(),
-			chatroom.getBoard().isDeleted());
+				partner.getName(), fileService.getProfileImage(partner.getId()), chatroom.getBoard().getStatus(),
+				chatroom.getBoard().getTitle(), chatroom.getBoard().getId(), chatroom.getBoard().getMember().getId(),
+				chatroom.getBoard().isDeleted());
 	}
 
 }
