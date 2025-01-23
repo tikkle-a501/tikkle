9521e2f86de2e4f08c5fb11c397006babda3438f
HurDong <gjehdals456@gmail.com>
2025-01-23 23:59:55 +0900

    feat: consumer 기본 메서드 추가
    #5

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/chat/service/KafkaConsumer.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/chat/service/KafkaConsumer.java
@@ -8,6 +8,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taesan.tikkle.domain.chat.dto.response.ChatMessageResponse;
 
@@ -31,4 +32,17 @@ public class KafkaConsumer {
 	// 	// STOMP 메시지 전송
 	// 	simpMessagingTemplate.convertAndSend("/topic/chatroom." + record.topic(), chatMessage);
 	// }
+	@KafkaListener(topicPattern = "chatroom.*", groupId = "")
+	public void consume(String message) {
+		// Kafka에 직렬화를 하고 message를 send 하였으므로 이를 다시 역직렬화하는 과정이 필요하다.
+		try {
+			// ObjectMapper를 사용하여 JSON 메시지를 ChatMessageResponse 객체로 변환
+			ChatMessageResponse response = objectMapper.readValue(message, ChatMessageResponse.class);
+			// 로그: 수신한 메시지 출력
+			logger.info("수신한 ChatMessageResponse: {}", response);
+		} catch (JsonProcessingException e) {
+			logger.error("JSON 직렬화 중 오류 발생: {}", e.getMessage());
+		}
+	}
+
 }
