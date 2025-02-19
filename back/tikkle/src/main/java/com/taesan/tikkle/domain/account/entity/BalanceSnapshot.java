5596a968711371f069acaf73c87a32e8cfe17dbf
glenn-syj <glenn.syj@gmail.com>
2025-02-19 23:52:39 +0900

    feat: BalanceSnapshot 빌더 패턴 적용

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/account/entity/BalanceSnapshot.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/account/entity/BalanceSnapshot.java
@@ -1,11 +1,13 @@
 package com.taesan.tikkle.domain.account.entity;
 
+import com.taesan.tikkle.domain.member.entity.Member;
 import com.taesan.tikkle.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -26,5 +28,11 @@ public class BalanceSnapshot extends BaseEntity {
 
     @Column(nullable = false)
     private int balance;
+
+    @Builder
+    private BalanceSnapshot(UUID accountId, int balance) {
+        this.accountId = accountId;
+        this.balance = balance;
+    }
 
 }
