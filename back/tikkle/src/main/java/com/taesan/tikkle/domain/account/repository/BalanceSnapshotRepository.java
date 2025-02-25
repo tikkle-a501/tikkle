749822aeee197fc0550c48c441039a28ae44c1e6
glenn-syj <glenn.syj@gmail.com>
2025-02-25 17:02:57 +0900

    feat: 시간 역순으로 계좌스냅샷 첫 엔티티 find 쿼리

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/account/repository/BalanceSnapshotRepository.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/account/repository/BalanceSnapshotRepository.java
@@ -5,8 +5,12 @@ import com.taesan.tikkle.domain.account.entity.BalanceSnapshot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BalanceSnapshotRepository extends JpaRepository<BalanceSnapshot, UUID> {
+
+    Optional<BalanceSnapshot> findFirstByAccountIdOrderByCreatedAtDesc(UUID accountId);
+
 }
