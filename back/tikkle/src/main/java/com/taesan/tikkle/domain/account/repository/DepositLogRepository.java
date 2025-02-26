676a7fbd6c8ef48c10a499b0dd1f9a3393a9b7be
glenn-syj <glenn.syj@gmail.com>
2025-02-27 00:00:22 +0900

    fix: 매핑 DepositLog로 수정

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/account/repository/DepositLogRepository.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/account/repository/DepositLogRepository.java
@@ -1,5 +1,6 @@
 package com.taesan.tikkle.domain.account.repository;
 
+import com.taesan.tikkle.domain.account.entity.DepositLog;
 import com.taesan.tikkle.domain.account.entity.ExchangeLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface DepositLogRepository extends JpaRepository<ExchangeLog, UUID> {
+public interface DepositLogRepository extends JpaRepository<DepositLog, UUID> {
 
     List<ExchangeLog> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
