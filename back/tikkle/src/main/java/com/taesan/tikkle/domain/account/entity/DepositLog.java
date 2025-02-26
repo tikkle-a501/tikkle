63021be6d17c96735f5711839fcf8ecefb2bfdac
glenn-syj <glenn.syj@gmail.com>
2025-02-26 23:50:34 +0900

    feat: 약속 상태 변경 시 계좌 자산 추적 로그 엔티티 생성

---
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/account/entity/DepositLog.java
@@ -0,0 +1,35 @@
+package com.taesan.tikkle.domain.account.entity;
+
+import com.taesan.tikkle.domain.account.dto.DepositType;
+import com.taesan.tikkle.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "deposit_logs")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DepositLog extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @Column(nullable = false)
+    private Integer amount;
+
+    @Enumerated(EnumType.STRING)
+    private DepositType type; // DEPOSIT(보증금 차감), REFUND(보증금 반환)
+
+    public DepositLog(Account account, Integer amount, DepositType type) {
+        this.account = account;
+        this.amount = amount;
+        this.type = type;
+    }
+}
+
