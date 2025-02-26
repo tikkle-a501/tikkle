4ae08cc165638643c5a491216493cb4ae256ae9b
glenn-syj <glenn.syj@gmail.com>
2025-02-26 23:53:25 +0900

    feat: DepositLog 레포지토리 추가
    - 당일 내의 로그를 확인하기 위한 메서드 추가

---
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/account/repository/DepositLogRepository.java
@@ -0,0 +1,15 @@
+package com.taesan.tikkle.domain.account.repository;
+
+import com.taesan.tikkle.domain.account.entity.ExchangeLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface DepositLogRepository extends JpaRepository<ExchangeLog, UUID> {
+
+    List<ExchangeLog> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
+}
